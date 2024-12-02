f3faeef49466b2a1594933108a097eda915db42b
rahulpandharkar <rahulpandharkar@hotmail.com>
2024-12-03 04:07:56 +0530

    Notifications fixed, positioned well to trigger on checkin/checkout, needs formatting and dynamic information in terms of location and ACTION

--- lib/firestore_service.dart
+++ lib/firestore_service.dart
@@ -2,9 +2,73 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http; 
+import 'package:googleapis_auth/auth_io.dart' as auth;
+import 'package:googleapis/servicecontrol/v1.dart' as servicecontrol;  
+import 'dart:convert'; 
+import 'package:flutter/services.dart' show rootBundle;
 
 class FirestoreService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> getServerToken() async {
+  final jsonString = await rootBundle.loadString('assets/fcm_access_token/service_token.json');
+  final serviceAccountJson = jsonDecode(jsonString);
+
+  List<String> scopes = [
+    "https://www.googleapis.com/auth/firebase.messaging"
+  ];
+
+  // Create an HTTP client
+  http.Client client = await auth.clientViaServiceAccount(
+    auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
+    scopes,
+  );
+
+  // Obtain access credentials
+  auth.AccessCredentials credentials = await auth.obtainAccessCredentialsViaServiceAccount(
+    auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
+    scopes,
+    client,
+  );
+
+  client.close();
+
+  // Return the access token
+  return credentials.accessToken.data;
+  }
+
+  Future<void> sendNotification(String email, String location, String timestamp) async {
+    print("**********************************************************SEND NOTIFICATION CALLED****************************************************"); 
+    var deviceToken = "e4AmFTEASL6yid_ha__lqd:APA91bE0NttTwBV6expXSRwUWAxHbhPZcxQxqT6lEYa5T9YyPQ6w6h26eWnuKLi85xFs4ujTaVhKuCCcXqD1P7a4I9S-Hreqd0yIU2d1jynrJI5LVA5LQIM";
+    final String serverKey = await getServerToken();
+    String endpoint = "https://fcm.googleapis.com/v1/projects/employee-app-1fd50/messages:send";
+
+    final Map<String, dynamic> message = {
+      'message': {
+        'token': deviceToken,
+        'notification': {
+          'title': "Check-in Notification",
+          'body': "$email checked in at $location on $timestamp!",
+        },
+      },
+    };
+
+    final http.Response response = await http.post(
+      Uri.parse(endpoint),
+      headers: <String, String>{
+        'Content-Type': "application/json",
+        'Authorization': 'Bearer $serverKey',
+      },
+      body: jsonEncode(message),
+    );
+
+    if (response.statusCode == 200) {
+      print("Notification sent successfully");
+    } else {
+      print("Failed to send notification: ${response.body}");
+    }
+  }
 
   // Generic function to save data (check-in or check-out)
   Future<void> saveData(Position position, String action) async {
@@ -28,6 +92,7 @@ class FirestoreService {
         'action': action,
         'notification_read': false,
       });
+      sendNotification(user.email!, "location", DateTime.now().toString());
     } catch (e) {
       print("Error saving $action data: $e");
     }
--- lib/notifications.dart
+++ lib/notifications.dart
@@ -2,11 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:geocoding/geocoding.dart';
-import 'package:http/http.dart' as http; 
-import 'package:googleapis_auth/auth_io.dart' as auth;
-import 'package:googleapis/servicecontrol/v1.dart' as servicecontrol;  
-import 'dart:convert'; 
-import 'package:flutter/services.dart' show rootBundle;
 
 class NotificationsScreen extends StatefulWidget {
   const NotificationsScreen({Key? key}) : super(key: key);
@@ -16,67 +11,7 @@ class NotificationsScreen extends StatefulWidget {
 }
 
 class _NotificationsScreenState extends State<NotificationsScreen> {
-    bool isNotificationSent = false;
     //Server token for google fcm
-    Future<String> getServerToken() async {
-    final jsonString = await rootBundle.loadString('assets/fcm_access_token/service_token.json');
-  final serviceAccountJson = jsonDecode(jsonString);
-
-  List<String> scopes = [
-    "https://www.googleapis.com/auth/firebase.messaging"
-  ];
-
-  // Create an HTTP client
-  http.Client client = await auth.clientViaServiceAccount(
-    auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
-    scopes,
-  );
-
-  // Obtain access credentials
-  auth.AccessCredentials credentials = await auth.obtainAccessCredentialsViaServiceAccount(
-    auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
-    scopes,
-    client,
-  );
-
-  client.close();
-
-  // Return the access token
-  return credentials.accessToken.data;
-  }
-
-  Future<void> sendNotification(String email, String location, String timestamp) async {
-    print("**********************************************************SEND NOTIFICATION CALLED****************************************************"); 
-    var deviceToken = "e4AmFTEASL6yid_ha__lqd:APA91bE0NttTwBV6expXSRwUWAxHbhPZcxQxqT6lEYa5T9YyPQ6w6h26eWnuKLi85xFs4ujTaVhKuCCcXqD1P7a4I9S-Hreqd0yIU2d1jynrJI5LVA5LQIM";
-    final String serverKey = await getServerToken();
-    String endpoint = "https://fcm.googleapis.com/v1/projects/employee-app-1fd50/messages:send";
-
-    final Map<String, dynamic> message = {
-      'message': {
-        'token': deviceToken,
-        'notification': {
-          'title': "Check-in Notification",
-          'body': "$email checked in at $location on $timestamp!",
-        },
-      },
-    };
-
-    final http.Response response = await http.post(
-      Uri.parse(endpoint),
-      headers: <String, String>{
-        'Content-Type': "application/json",
-        'Authorization': 'Bearer $serverKey',
-      },
-      body: jsonEncode(message),
-    );
-
-    if (response.statusCode == 200) {
-      print("Notification sent successfully");
-    } else {
-      print("Failed to send notification: ${response.body}");
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -148,8 +83,6 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
                   }
 
                   String location = locationSnapshot.data ?? 'Unknown Location';
-                  sendNotification(userEmail ?? "Unknown", location, formattedTimestamp);
-
                   return Card(
                     margin: const EdgeInsets.all(8.0),
                     child: ListTile(
